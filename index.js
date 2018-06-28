c2cc2821019901e1012e2b3ec5ed1febb25ec778
BloodyLink <david.quezada.m@gmail.com>
2018-06-28 09:51:29 -0400

    Se cambia mensaje al iniciar

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-var SlackBot = require("slackbots");
+var SlackBot = require('slackbots');
 
 const envKey = process.env.MONOBOT_TOKEN;
 
 // create a bot
 var bot = new SlackBot({
   token: envKey, // Add a bot https://my.slack.com/services/new/bot and put the token
-  name: "monobot"
+  name: 'monobot'
 });
 
-bot.on("start", function() {
+bot.on('start', function() {
   // more information about additional params https://api.slack.com/methods/chat.postMessage
   var params = {
-    icon_emoji: ":speak_no_evil:"
+    icon_emoji: ':speak_no_evil:'
   };
 
   // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-  bot.postMessageToChannel("general", "uh! uh! uh!", params);
+  bot.postMessageToChannel('general', 'uh! uh! Estoy listo!', params);
 
   // define existing username instead of 'user_name'
-  bot.postMessageToUser("user_name", "uh! uh! uh!", params);
+  bot.postMessageToUser('user_name', 'uh! uh! uh!', params);
 
   // If you add a 'slackbot' property,
   // you will post to another user's slackbot channel instead of a direct message
-  bot.postMessageToUser("user_name", "uh! uh! uh!", {
+  bot.postMessageToUser('user_name', 'uh! uh! uh!', {
     slackbot: true,
-    icon_emoji: ":speak_no_evil:"
+    icon_emoji: ':speak_no_evil:'
   });
 
   // define private group instead of 'private_group', where bot exist
-  bot.postMessageToGroup("private_group", "uh! uh! uh!", params);
+  bot.postMessageToGroup('private_group', 'uh! uh! uh!', params);
 
   //the fun begins...
 
-  bot.on("message", msg => {
+  bot.on('message', msg => {
     switch (msg.type) {
-      case "message":
-        if (msg.channel[0] === "D" && msg.bot_id === undefined) {
+      case 'message':
+        if (msg.channel[0] === 'D' && msg.bot_id === undefined) {
           let text = msg.text.toLowerCase();
 
           switch (true) {
-            case text.includes("hola"):
-              bot.postMessage(msg.user, "hola po", { as_user: true });
+            case text.includes('hola'):
+              bot.postMessage(msg.user, 'hola po', { as_user: true });
               break;
-            case text.includes("chao"):
-              bot.postMessage(msg.user, "chao po!", { as_user: true });
+            case text.includes('chao'):
+              bot.postMessage(msg.user, 'chao po!', { as_user: true });
               break;
-            case text.includes("cuando pagan"):
+            case text.includes('cuando pagan'):
               bot.postMessage(
                 msg.user,
-                "El último día habil del mes po, supongo.",
+                'El último día habil del mes po, supongo.',
                 { as_user: true }
               );
               break;
