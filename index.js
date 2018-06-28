ac9a699c362c55c94b1756b1f1c3602527b2a27f
BloodyLink <david.quezada.m@gmail.com>
2018-06-28 02:54:30 -0400

    Se comienzan a agregar mensajes por DM

--- index.js
+++ index.js
@@ -29,4 +29,39 @@ bot.on("start", function() {
 
   // define private group instead of 'private_group', where bot exist
   bot.postMessageToGroup("private_group", "uh! uh! uh!", params);
+
+  //the fun begins...
+
+  bot.on("message", msg => {
+    switch (msg.type) {
+      case "message":
+        if (msg.channel[0] === "D" && msg.bot_id === undefined) {
+          let text = msg.text.toLowerCase();
+
+          switch (true) {
+            case text.includes("hola"):
+              bot.postMessage(msg.user, "hola po", { as_user: true });
+              break;
+            case text.includes("chao"):
+              bot.postMessage(msg.user, "chao po!", { as_user: true });
+              break;
+            case text.includes("cuando pagan"):
+              bot.postMessage(
+                msg.user,
+                "El último día habil del mes po, supongo.",
+                { as_user: true }
+              );
+              break;
+            /**
+             * TODO: Vamos a agregarle una api con los feriados para calcular el ultmo dia habil del mes.
+             *
+             * Que mas le podemos incluir?
+             *
+             * Copiamos a coipo??
+             */
+          }
+        }
+        break;
+    }
+  });
 });
